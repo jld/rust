831bf63f77b605a073017dc3e2dd8309100233c3
Jed Davis <jld@panix.com>
2013-03-24 21:50:30 -0700

    A first attempt at Rust paragraph filling.

--- src/etc/emacs/new-rust-mode.el
+++ src/etc/emacs/new-rust-mode.el
@@ -39,6 +39,77 @@
 
     ))
 
+(defun new-rust-fill-paragraph (&optional justify)
+  ;; XXX the searches in here do the wrong thing in some cases.
+  ;; Also this is far too complicated-looking for what it does.
+  ;; Furthermore, it behaves suboptimally if run outside a comment.
+  (interactive "*P")
+  (save-excursion
+    (beginning-of-line)
+    (let* ((start (point))
+	   (end (point-at-eol))
+	   (is-line-comments (prog1 (search-forward "//" end t) (beginning-of-line))))
+      (cond
+       (is-line-comments
+	; Skip ahead over lines that are just //
+	(beginning-of-line)
+	(while (and (not (re-search-forward "[^[:space:]/]" (point-at-eol) t))
+		    (search-forward "//" (point-at-eol) t)
+		    (= (forward-line 1) 0)))
+	; If that moved us forward, then we know where the start is.
+	(if (/= start (point-at-bol))
+	    (setq start (point-at-bol))
+	  ; Otherwise, find the beginning.
+	  (save-excursion
+	    (beginning-of-line)
+	    (while (and (= (forward-line -1) 0)
+			(re-search-forward "[^[:space:]]" (point-at-eol) t)
+			(eq (char-before) ?/)
+			(eq (char-after) ?/)
+			(re-search-forward "[^[:space:]/]" (point-at-eol) t)
+			(setq start (point-at-bol))))))
+	; Find the end of the comment or paragraph
+	(while (and (= (forward-line 1) 0)
+		    (re-search-forward "[^[:space:]]" (point-at-eol) t)
+		    (eq (char-before) ?/)
+		    (eq (char-after) ?/)
+		    (re-search-forward "[^[:space:]/]" (point-at-eol) t)
+		    (setq end (point-at-eol)))))
+       ; Otherwise, it's a block comment:
+       (t
+	; Skip ahead over lines that are blank or just a *.
+	(while (and (not (re-search-forward "[^[:space:]*]" (point-at-eol) t))
+		    (= (forward-line 1) 0)))
+	; If that moved us forward, then we know where the start is.
+	(if (/= start (point-at-bol))
+	    (setq start (point-at-bol))
+	  ; Otherwise, find the beginning.
+	  (save-excursion
+	    (beginning-of-line)
+	    (while (and (not (search-forward "/*" (point-at-eol) t))
+			(re-search-forward "[^[:space:]*]" (point-at-eol) t)
+			(= (forward-line -1) 0)))
+	    ; Use the first line only if it's more than just /*
+	    (beginning-of-line)
+	    (if (re-search-forward "[^[:space:]*/]" (point-at-eol) t)
+		(setq start (point-at-bol))
+	      (setq start (point-at-eol)))))
+	; Find the end of the comment or paragraph
+	(beginning-of-line)
+	(while (and (not (search-forward "*/" (point-at-eol) t))
+			(re-search-forward "[^[:space:]*]" (point-at-eol) t)
+			(= (forward-line 1) 0)))
+	; Use the last line only if it's more than just */
+	(beginning-of-line)
+	(if (re-search-forward "[^[:space:]*/]" (point-at-eol) t)
+	    (setq end (point-at-eol))
+	  (setq end (point-at-bol)))))
+      ; Finally, do the actual fill.
+      (let ((adaptive-fill-regexp ;; NOTE dynamic scope let
+	     (concat
+	      (if is-line-comments "\\(\s *//+\\)?" "")
+	      adaptive-fill-regexp)))
+	(fill-region start end justify)))))
 
 ;;;###autoload
 (define-derived-mode new-rust-mode prog-mode "New Rust"
@@ -47,12 +118,13 @@
   (run-hooks 'new-rust-mode-hook)
   (set (make-local-variable 'font-lock-defaults) 
        '(new-rust-font-lock-keywords))
+
   (set (make-local-variable 'fill-paragraph-function)
-       'fill-comment-paragraph)
+       'new-rust-fill-paragraph)
+
   (set (make-local-variable 'comment-start) "/* ")
   (set (make-local-variable 'comment-start-skip) "\\(//+\\|/\\*+\\)\s *")
   (set (make-local-variable 'comment-end) " */")
-
   (set (make-local-variable 'comment-style) 'extra-line) ; controversial?
   (set (make-local-variable 'comment-multi-line) t)
 
